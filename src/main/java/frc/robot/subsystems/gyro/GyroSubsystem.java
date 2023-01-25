3160333f7d908617ef3fb65b916392dc23da95b9
Krishiv Manyam <krishivmanyam@gmail.com>
2023-01-25 13:27:16 -0800

    Relocated Gyro Subsystem

--- src/main/java/frc/robot/subsystems/gyro/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/gyro/GyroSubsystem.java
@@ -1,35 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.gyro;
 
+
+
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.kauailabs.navx.frc.AHRS;
-import frc.robot.Constants;
-
-
 public class GyroSubsystem extends SubsystemBase {
-public Constants constant = new Constants();
-  public AHRS ahrs;
-  
+
+
   public GyroSubsystem() {
+    SmartDashboard.putString("subsystem", "init gyro sub");
+  }
+  AHRS ahrs = new AHRS();  
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber(   "Yaw", ahrs.getYaw());
+    SmartDashboard.putNumber(   "Pitch", ahrs.getPitch());
+    SmartDashboard.putNumber(   "Roll", ahrs.getRoll());
+  }
+  public double getAngle(){
+    return ahrs.getAngle();
+}
 
-        SmartDashboard.putString("subsystem", "init gyro sub");
-        try {
-	           ahrs = new AHRS(Constants.SerialPortAHRS); 
-				} catch (RuntimeException ex ) {
-            DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true); 
-        }
-
-        SmartDashboard.putNumber("init angle", ahrs.getAngle());
-    }
-
-    public double getGyroAngle(){
-        return ahrs.getAngle();
-    }
-
-    @Override
-    public void periodic(){
-        SmartDashboard.putNumber("gyro", this.getGyroAngle());
-    }
 }
