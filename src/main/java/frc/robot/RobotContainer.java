e3fca130ef5cef49134b041560d552aa492e025d
Krishiv Manyam <krishivmanyam@gmail.com>
2023-02-08 13:05:49 -0800

    fixed imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,23 +1,11 @@
 package frc.robot;
 
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.UsbCamera;
-import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.drive.ArcadeDrive;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
-import frc.robot.subsystems.intake.IntakeSubsystem;
 
 public class RobotContainer {
   private final XboxController joystick1 = new XboxController(0); //driver
@@ -52,6 +40,7 @@ public class RobotContainer {
   }
 
   public void setDefaultCommands() {
+    
     driveBaseSubsystem.setDefaultCommand(arcadeDrive);
   }
 }
