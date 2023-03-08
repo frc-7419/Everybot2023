28b6e8c5a5b033685e191818aac525bfaf0352d5
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2023-03-08 15:12:29 -0800

    Updated Auton

--- src/main/java/frc/robot/autoncommands/ScorePreload.java
+++ src/main/java/frc/robot/autoncommands/ScorePreload.java
@@ -9,7 +9,7 @@ import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.RaiseArm;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.MoveToSetpoint;
-import frc.robot.subsystems.encoder.EncoderSubsystem;
+// import frc.robot.subsystems.encoder.EncoderSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -17,11 +17,11 @@ import frc.robot.subsystems.gyro.GyroSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ScorePreload extends ParallelCommandGroup {
   /** Creates a new Bruh. */
-  public ScorePreload(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, EncoderSubsystem encoderSubsystem, ArmSubsystem armSubsystem) {
+  public ScorePreload(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ArmSubsystem armSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new MoveToSetpoint(driveBaseSubsystem, 0.6096, encoderSubsystem),
+      new MoveToSetpoint(driveBaseSubsystem, 0.6096),
       new RaiseArm(armSubsystem)
       //test
     );
