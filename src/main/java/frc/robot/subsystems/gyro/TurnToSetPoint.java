3bd9fd7d10c62f322bf711dc0a5d13fd0e7695cc
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2023-03-08 14:51:34 -0800

    Auton Code Fixed
    Fixed auton

--- src/main/java/frc/robot/subsystems/gyro/TurnToSetPoint.java
+++ src/main/java/frc/robot/subsystems/gyro/TurnToSetPoint.java
@@ -2,32 +2,36 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.drive;
+package frc.robot.subsystems.gyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.fasterxml.jackson.databind.deser.impl.SetterlessProperty;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.LowerArm;
 import frc.robot.subsystems.arm.RaiseArm;
+import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.encoder.EncoderSubsystem;
+import frc.robot.subsystems.gyro.GyroSubsystem;;
 
 
-public class MoveToSetpoint extends CommandBase {
+public class TurnToSetPoint extends CommandBase {
   private double leftPower;
   private double rightPower;
   private EncoderSubsystem Es;
   public double setpoint;
   private DriveBaseSubsystem driveBaseSubsystem ;
+  private GyroSubsystem gyroSubsystem;
   /** Creates a new StraightMotionMagic. */
-  public MoveToSetpoint(DriveBaseSubsystem driveBaseSubsystem,double setpoint, EncoderSubsystem Es) {
+  public TurnToSetPoint(DriveBaseSubsystem driveBaseSubsystem,double setpoint, GyroSubsystem gyroSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.setpoint = setpoint;
     this.driveBaseSubsystem = driveBaseSubsystem;
-    this.Es = Es;
+    this.gyroSubsystem = gyroSubsystem;
     leftPower = -0.25;
     rightPower = -0.25;
     Es = new EncoderSubsystem();
@@ -43,9 +47,9 @@ public class MoveToSetpoint extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    while(Es.encoder.getDistance()<setpoint){
-      driveBaseSubsystem.setLeftPower(leftPower);
-    driveBaseSubsystem.setLeftPower(rightPower);
+    double angle = gyroSubsystem.getAngle();
+    while(gyroSubsystem.getAngle() < angle+setpoint){
+      driveBaseSubsystem.setRightPower(rightPower);
     }
   }
 
@@ -59,3 +63,5 @@ public class MoveToSetpoint extends CommandBase {
     return false;
   }
 }
+
+  
