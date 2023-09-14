0fe4e7158da809a99b72b2e864015b9a8f41513b
Jolteous <wirelesskhurram@outlook.com>
2023-09-14 16:30:26 -0700

    Fixed 4.0

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,8 +16,6 @@ import frc.robot.subsystems.gyro.AutoDockBangBang;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.RunIntakeWithJoystick;
-import frc.robot.subsystems.intake.GroundIntakeSubsystem;
-import frc.robot.subsystems.intake.RunGroundIntake;
 
 public class RobotContainer {
   private final XboxController driver = new XboxController(0); //driver
@@ -27,13 +25,11 @@ public class RobotContainer {
   private final DriveTrainPoseSubsystem driveTrainPoseSubsystem = new DriveTrainPoseSubsystem(gyroSubsystem, driveBaseSubsystem);
   private final ArmSubsystem armSubsystem = new ArmSubsystem();
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  private final GroundIntakeSubsystem groundIntakeSubsystem = new GroundIntakeSubsystem();
   private final RunArmWithJoystick runArmWithJoystick = new RunArmWithJoystick(operator, armSubsystem);
   private final RunIntakeWithJoystick runIntakeWithJoystick = new RunIntakeWithJoystick(intakeSubsystem, driver);
 
   DriveBaseSubsystem swerveDrive = new DriveBaseSubsystem();
 
-  private final RunGroundIntake runGroundIntake = new RunGroundIntake(groundIntakeSubsystem, driver);
 
   private final ArcadeDrive arcadeDrive = new ArcadeDrive(driver, driveBaseSubsystem);
   
@@ -58,6 +54,5 @@ public class RobotContainer {
     swerveDrive.setDefaultCommand(new SwerveDriveFieldCentric(driver, swerveDrive, gyroSubsystem));
     //driveBaseSubsystem.setDefaultCommand(arcadeDrive);
     armSubsystem.setDefaultCommand(runArmWithJoystick);
-    GroundIntakeSubsystem.setDefaultCommand(runGroundIntake);
   }
 }
