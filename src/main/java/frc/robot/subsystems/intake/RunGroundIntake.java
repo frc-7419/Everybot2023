cc7ad868fad8af5093ce3d4498a93b23430d6285
Jolteous <wirelesskhurram@outlook.com>
2023-09-14 09:45:32 -0700

    Ground Intake DONE

---
+++ src/main/java/frc/robot/subsystems/intake/RunGroundIntake.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.intake;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.GroundIntakeSubsystem;
+import frc.robot.Constants;
+
+public class RunGroundIntake extends CommandBase {
+    private GroundIntakeSubsystem groundIntakeSubsystem
+    private XboxController joystick;
+
+    @Override
+    public runIntakeWithJoystick(GroundIntakeSubsystem groundIntakeSubsystem, XboxController joystick) {
+        this.groundIntakeSubsystem = groundIntakeSubsystem;
+        this.joystick = joystick;
+        
+    }
+    
+    @Override
+    public void initialize(){
+        groundIntakeSubsystem.setPower(0)
+    }
+
+    @Override
+    public void runIntake(double power) {
+        groundIntakeSubsystem.coast();
+        groundIntakeSubsystem.setPower(power);
+    }
+
+    @Override
+    public void execute() {
+        if (joystick.getLeftTriggerAxis > 0) {
+            runIntake(Constants.IntakePower);
+        }
+        else if (joystick.getRightTriggerAxis > 0) {
+            runIntake(-Constants.IntakePower);
+
+        }
+        else {
+            groundIntakeSubsystem.setPower(0);
+            groundIntakeSubsystem.brake();
+        }
+    }
+
+    @Override
+    public void isFinished(){
+        return False;
+    }
+
+    
+}
