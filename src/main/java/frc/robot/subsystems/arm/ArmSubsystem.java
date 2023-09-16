35167a80eaa2db3b259d6448078b07ca6479634e
Joshua Yang <jyangftw@gmail.com>
2023-09-15 20:33:54 -0700

    Clean EVERYTHING UNNECESSARY (AT THIS STAGE)

--- src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -12,7 +12,7 @@ import frc.robot.Constants.CanIds;
 public class ArmSubsystem extends SubsystemBase {
   private TalonFX arm;
   public ArmSubsystem() {
-    arm = new TalonFX(CanIds.leftFalcon1.id);
+    arm = new TalonFX(CanIds.arm.id);
     arm.setInverted(true);
     arm.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
     arm.setSelectedSensorPosition(0);
