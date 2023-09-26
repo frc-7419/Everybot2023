c236416745255431f9717b1644251fcb21e562fb
Jolteous <wirelesskhurram@outlook.com>
2023-09-26 16:44:08 -0700

    fixed
    Co-Authored-By: Glue <91361677+ItsGlue@users.noreply.github.com>
    Co-Authored-By: AnmingZhang <113463512+AnmingZhang@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,10 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmToPosition;
 import frc.robot.subsystems.arm.RunArmWithJoystick;
-import frc.robot.subsystems.drive.ArcadeDrive;
-import frc.robot.subsystems.drive.DriveBaseSubsystem;
 
-import frc.robot.subsystems.gyro.AutoDockBangBang;
+// import frc.robot.subsystems.drive.DriveBaseSubsystem;
+
+// import frc.robot.subsystems.gyro.AutoDockBangBang;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.intake.GroundIntakeSubsystem;
 import frc.robot.subsystems.intake.RunGroundIntakeWithJoystick;
@@ -18,7 +18,7 @@ import frc.robot.subsystems.intake.RunGroundIntakeWithJoystick;
 public class RobotContainer {
   private final XboxController driver = new XboxController(0); //driver
   private final XboxController operator = new XboxController(1); //operator
-  private final DriveBaseSubsystem driveBaseSubsystem = new DriveBaseSubsystem();
+  // private final DriveBaseSubsystem driveBaseSubsystem = new DriveBaseSubsystem();
   private final GyroSubsystem gyroSubsystem = new GyroSubsystem();
   
   private final ArmSubsystem armSubsystem = new ArmSubsystem();
@@ -26,7 +26,7 @@ public class RobotContainer {
   private final RunArmWithJoystick runArmWithJoystick = new RunArmWithJoystick(operator, armSubsystem);
   private final RunGroundIntakeWithJoystick runGroundIntakeWithJoystick = new RunGroundIntakeWithJoystick(groundIntakeSubsystem, driver);
 
-  private final ArcadeDrive arcadeDrive = new ArcadeDrive(driver, driveBaseSubsystem);
+  
   
 
   public RobotContainer() {
@@ -34,11 +34,11 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
-    new JoystickButton(driver, XboxController.Button.kA.value)
-      .whileTrue(new AutoDockBangBang(gyroSubsystem, driveBaseSubsystem));
+    // new JoystickButton(driver, XboxController.Button.kA.value)
+    //   .whileTrue(new AutoDockBangBang(gyroSubsystem, driveBaseSubsystem));
 
-    new JoystickButton(driver, XboxController.Button.kB.value)
-      .whileTrue(new ArmToPosition(armSubsystem, 5000));
+    // new JoystickButton(driver, XboxController.Button.kB.value)
+    //   .whileTrue(new ArmToPosition(armSubsystem, 5000));
     
   
   }
@@ -48,7 +48,6 @@ public class RobotContainer {
   }
 
   public void setDefaultCommands() {
-    driveBaseSubsystem.setDefaultCommand(arcadeDrive);
     armSubsystem.setDefaultCommand(runArmWithJoystick);
     groundIntakeSubsystem.setDefaultCommand(runGroundIntakeWithJoystick);
   }
--- src/main/java/frc/robot/subsystems/gyro/TurnWithGyroClosedLoop.java
+++ src/main/java/frc/robot/subsystems/gyro/TurnWithGyroClosedLoop.java
@@ -1,82 +1,82 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.gyro;
+// package frc.robot.subsystems.gyro;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.math.controller.PIDController;
-import frc.robot.subsystems.drive.DriveBaseSubsystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import edu.wpi.first.math.controller.PIDController;
+// import frc.robot.subsystems.drive.DriveBaseSubsystem;
 
-public class TurnWithGyroClosedLoop extends CommandBase {
-  private DriveBaseSubsystem driveBaseSubsystem;
- private  GyroSubsystem gyroSubsystem;
- private  double target;
- private  double tolerance;
- private  double kP;
- private  double kI;
- private  double kD;
- private  PIDController pidController;
- private  double pidOutput;
- private  double initAngle;
+// public class TurnWithGyroClosedLoop extends CommandBase {
+//   private DriveBaseSubsystem driveBaseSubsystem;
+//  private  GyroSubsystem gyroSubsystem;
+//  private  double target;
+//  private  double tolerance;
+//  private  double kP;
+//  private  double kI;
+//  private  double kD;
+//  private  PIDController pidController;
+//  private  double pidOutput;
+//  private  double initAngle;
 
-  /** Creates a new TurnWithGyroClosedLoop. */
-  public TurnWithGyroClosedLoop(DriveBaseSubsystem driveBaseSubsystem,
-   GyroSubsystem gyroSubsystem,
-   double target,
-   double tolerance,
-   double kP,
-   double kI,
-   double kD,
-   PIDController pidController,
-   double pidOutput,
-   double initAngle) {
-    this.driveBaseSubsystem = driveBaseSubsystem;
-    this.gyroSubsystem = gyroSubsystem;
-    this.target = target;
-    this.kP = kP;
-    this.kI = kI;
-    this.kD = kD;
-    this.tolerance = tolerance;
-    this.pidOutput = pidOutput;
-    this.initAngle = initAngle;
-    this.pidController = pidController;
+//   /** Creates a new TurnWithGyroClosedLoop. */
+//   public TurnWithGyroClosedLoop(DriveBaseSubsystem driveBaseSubsystem,
+//    GyroSubsystem gyroSubsystem,
+//    double target,
+//    double tolerance,
+//    double kP,
+//    double kI,
+//    double kD,
+//    PIDController pidController,
+//    double pidOutput,
+//    double initAngle) {
+//     this.driveBaseSubsystem = driveBaseSubsystem;
+//     this.gyroSubsystem = gyroSubsystem;
+//     this.target = target;
+//     this.kP = kP;
+//     this.kI = kI;
+//     this.kD = kD;
+//     this.tolerance = tolerance;
+//     this.pidOutput = pidOutput;
+//     this.initAngle = initAngle;
+//     this.pidController = pidController;
 
 
-    //TODOContinue/FINISH THIS
-  }
+//     //TODOContinue/FINISH THIS
+//   }
   
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    driveBaseSubsystem.coast();
-    initAngle = gyroSubsystem.getYaw();
-    pidController = new PIDController(kP, kI, kD);
-    pidController.setTolerance(tolerance);
-    pidController.setSetpoint(initAngle + target);
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {
+//     driveBaseSubsystem.coast();
+//     initAngle = gyroSubsystem.getYaw();
+//     pidController = new PIDController(kP, kI, kD);
+//     pidController.setTolerance(tolerance);
+//     pidController.setSetpoint(initAngle + target);
 
-    //TODOContinue/FINISH THIS
+//     //TODOContinue/FINISH THIS
   
-  }
+//   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    pidOutput = pidController.calculate(gyroSubsystem.getYaw());
-    driveBaseSubsystem.setLeftPower(pidOutput);
-    driveBaseSubsystem.setRightPower(-pidOutput);
-  }
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     pidOutput = pidController.calculate(gyroSubsystem.getYaw());
+//     driveBaseSubsystem.setLeftPower(pidOutput);
+//     driveBaseSubsystem.setRightPower(-pidOutput);
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    driveBaseSubsystem.stop(); 
-    driveBaseSubsystem.brake();
-  }
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {
+//     driveBaseSubsystem.stop(); 
+//     driveBaseSubsystem.brake();
+//   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return pidController.atSetpoint();
-  }
-}
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return pidController.atSetpoint();
+//   }
+// }
