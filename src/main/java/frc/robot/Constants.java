1efc831416feb5c269653f34d2cbd597a3700e09
Krishiv Manyam <krishivmanyam@gmail.com>
2023-10-24 15:12:28 -0700

    Added ArmOuttake Command for use in command groups

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,7 +61,8 @@ public final class Constants {
   public static class PowerConstants {
       public static final double groundIntakePower = 0.2; //arbitrary for now
       public static final double ArmPower = 0.2;//arbitrary for now
-      public static final double ArmIntakeSpeed = 0.5; //arbitrary for now
+      public static final double ArmIntakeSpeed = 0.2; //arbitrary for now - slower speed for testing
+    public static final double ArmOuttakeSpeed = -0.2; //arbitrary for now
       public static double maxArmPower = 0.1;
   }
 
