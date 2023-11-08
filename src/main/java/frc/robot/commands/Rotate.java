312cbd9d4c5e0f7e7dd1654489e3ab51c8232918
Sepandar <sepandar24@gmail.com>
2023-11-08 12:52:38 -0800

    making a rotation command because auton command dont work
    Co-Authored-By: Jahaanshah Sheikh <113569013+Jolteous@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/Rotate.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drive.DriveBaseSubsystem;
+import frc.robot.subsystems.drive.SwerveDriveFieldCentric;
+
+public class Rotate extends CommandBase {
+  private double degrees;
+  private DriveBaseSubsystem driveBaseSubsystem;
+  private SwerveDriveFieldCentric swerveDriveFieldCentric;
+  private double startingYaw;
+
+  public Rotate(DriveBaseSubsystem driveBaseSubsystem, SwerveDriveFieldCentric swerveDriveFieldCentric,double degrees) {
+    this.driveBaseSubsystem = driveBaseSubsystem;
+    this.swerveDriveFieldCentric = swerveDriveFieldCentric;
+    this.degrees = degrees;
+    addRequirements(driveBaseSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    //add 180 to yaw because I said so
+    startingYaw = driveBaseSubsystem.getYaw() +180;
+  }
+
+  @Override
+  public void execute() {
+    if(driveBaseSubsystem.getYaw()-startingYaw > degrees) {
+      swerveDriveFieldCentric.setModuleStatesFromChassisSpeed(new ChassisSpeeds(0,0, Math.PI/2));
+    }
+    else {
+      swerveDriveFieldCentric.setModuleStatesFromChassisSpeed(new ChassisSpeeds(0,0, -Math.PI/2));
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    swerveDriveFieldCentric.setModuleStatesFromChassisSpeed(new ChassisSpeeds(0,0,0));
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/commands/TwoPiece.java
+++ src/main/java/frc/robot/commands/TwoPiece.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.arm.ArmSetpointPID;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.RunArm;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
@@ -21,17 +22,16 @@ public class TwoPiece extends SequentialCommandGroup {
       // (new ArmWithPID(armSubsystem, 3.2, 0.422, 0.2).alongWith(new RunIntake(intakeSubsystem, 0.3))).withTimeout(1.5),
       // // new ArmWithPID(armSubsystem, 1, 0, 0.2).withTimeout(1.5), // TO TEST
       // new RunArm(armSubsystem, -0.15).withTimeout(4),
-      new RunArm(armSubsystem,-0.15).alongWith(new RunIntake(intakeSubsystem, 0.115)).withTimeout(5),
-      new RunArm(armSubsystem,-0.05).alongWith(new RunIntake(intakeSubsystem, -0.5)).withTimeout(1),
-      new WaitCommand(3),
+      new ArmSetpointPID(armSubsystem,0.481).alongWith(new RunIntake(intakeSubsystem, 0.115)).withTimeout(2),
+      new RunIntake(intakeSubsystem, -0.5).withTimeout(0.5),
+      new ArmSetpointPID(armSubsystem,0.144),
       // change the setpoint to whatever the proper setpoint is after runwrist is fixed
-      new RunWrist(groundIntakeSubsystem, 0),
+      new MoveForward(driveBaseSubsystem, swerveDriveFieldCentric, -0.3, 5.69).alongWith(
+        new RunWrist(groundIntakeSubsystem, 0)).alongWith(
+          new RunGroundIntake(groundIntakeSubsystem, 0.5)),
+      // change the setpoint to whatever the proper setpoint is after runwrist is fixed
+      new RunWrist(groundIntakeSubsystem, 90).alongWith(),
       new MoveForward(driveBaseSubsystem, swerveDriveFieldCentric, -0.3, 5.69),
-      // change to negative if it doesnt work
-      new RunGroundIntake(groundIntakeSubsystem, 0.5),
-      // change the setpoint to whatever the proper setpoint is after runwrist is fixed
-      new RunWrist(groundIntakeSubsystem, 90),
-      new MoveForward(driveBaseSubsystem, swerveDriveFieldCentric, 0.3, 5.69),
       // change the setpoint to whatever the proper setpoint is after runwrist is fixed
       new RunWrist(groundIntakeSubsystem, 0),
       // change to positive if it doesnt work
